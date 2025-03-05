467a2334651ec1fbca8fd57afc9d9b5e9232f893
MeghanaGumma <125593046+MeghanaGumma@users.noreply.github.com>
2025-03-05 20:02:06 +0530

    Delete 1.PythonBasics directory

--- 1.PythonBasics/1.3.data types.py
+++
@@ -1,6 +0,0 @@
-a=10
-b=10.1234567
-c='hello world'
-d=10.1234567890123
-e=True
-print(a,b,c,d,e,end=' ')
--- 1.PythonBasics/1.4.scope.py
+++
@@ -1,8 +0,0 @@
-x='global'
-
-def fn():
-    x='local'
-    print('inside the fn:',x)
-
-fn()
-print('outside the fn:',x)
